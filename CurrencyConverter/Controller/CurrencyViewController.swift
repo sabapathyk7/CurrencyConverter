4483ce10788fa5ae26744b7147372f3c345fca10
Kanagasabapathy <trksabapathy7@gmail.com>
2023-06-28 22:21:35 +0530

    Removed Main storyboard references
    Previous Commit -
    Removed main storyboard file from the project
    Removed references from the Build Settings of the project under the UIKit Main Storyboard File Base Name
    
    This Commit:
    Removed Storyboard Name from Info.plist file
    Programmatically set the root controller of our application by using UIWindowScene and assigned root View controller to the CurrencyViewController

--- CurrencyConverter/Controller/CurrencyViewController.swift
+++ CurrencyConverter/Controller/CurrencyViewController.swift
@@ -13,6 +13,7 @@ class CurrencyViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      self.view.backgroundColor = .blue
     }
     
 

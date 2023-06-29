d432938795639fa797893102bbda0cef3826cf16
Kanagasabapathy <trksabapathy7@gmail.com>
2023-06-29 15:36:36 +0530

    Modified ViewModel Deleted unused ViewController

--- CurrencyConverter/ViewModel/CurrencyDataSource.swift
+++ CurrencyConverter/ViewModel/CurrencyDataSource.swift
@@ -8,21 +8,29 @@
 import Foundation
 import UIKit
 
-class GenericDataSource<T> : NSObject {
-}
+class CurrencyDataSource<CELL : UITableViewCell,T> : NSObject, UITableViewDataSource {
 
-class CurrencyDataSource: GenericDataSource<Currency>, UITableViewDataSource{
+  private var cellIdentifier : String!
+  private var currencies : [T]!
+  var configureCell : (CELL, T) -> () = {_,_ in }
 
+  init(cellIdentifier: String, currencies: [T], configureCell: @escaping (UITableViewCell, T) -> Void) {
+    self.cellIdentifier = cellIdentifier
+    self.currencies = currencies
+    self.configureCell = configureCell
+  }
   func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return currencies.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyCell", for: indexPath) as! CurrencyCell
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyCell", for: indexPath) as! CELL
+    let currency = self.currencies[indexPath.row]
+    self.configureCell(cell,currency)
     return cell
   }
 }

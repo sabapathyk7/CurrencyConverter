6cfb8271420be74f4cecdb3a4cfcc2808447b79b
Kanagasabapathy <trksabapathy7@gmail.com>
2023-07-06 20:08:21 +0530

    Separated TableView
    Signed-off-by: Kanagasabapathy <trksabapathy7@gmail.com>

--- CurrencyConverter/Controller/CurrencyViewController.swift
+++ CurrencyConverter/Controller/CurrencyViewController.swift
@@ -10,17 +10,20 @@ import UIKit
 class CurrencyViewController: UIViewController {
 
   private let currencyViewModel:CurrencyViewModel = CurrencyViewModel()
-  private var currencyTableView: UITableView = UITableView()
+  private lazy var currencyTableView: UITableView = {
+    var tableView = UITableView()
+    let displayWidth: CGFloat = self.view.frame.width
+    let displayHeight: CGFloat = self.view.frame.height
+    tableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight))
+    tableView.register(CurrencyCell.self, forCellReuseIdentifier: "CurrencyCell")
+    view.addSubview(tableView)
+    return tableView
+  }()
   private var currencyData:CurrencyData?
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let displayWidth: CGFloat = self.view.frame.width
-    let displayHeight: CGFloat = self.view.frame.height
-    currencyTableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight))
-    currencyTableView.register(CurrencyCell.self, forCellReuseIdentifier: "CurrencyCell")
-    self.view.addSubview(currencyTableView)
     currencyTableView.delegate = self
     currencyTableView.dataSource = self
     currencyViewModel.callFetchCurrencyData { currencyData in

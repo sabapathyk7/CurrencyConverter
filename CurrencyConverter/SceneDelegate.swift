18bd53fc170daa5ca99e43d384814497d7c4fcd9
Kanagasabapathy <trksabapathy7@gmail.com>
2023-08-10 22:12:19 +0530

    - Created SwiftUIController and added to the ViewController group - Added UIHostingController and added contentview of SwiftUI and added to view hierarchy - Added hostingController to view's child - Added constraints to the swiftUIView to the ViewController - Updated SceneDelegate class to add the CurrencySwiftUIController as root view controller to the window.

--- CurrencyConverter/SceneDelegate.swift
+++ CurrencyConverter/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
-    let viewController = CurrencyViewController()
+    let viewController = CurrencySwiftUIViewController()
+//    let viewController = CurrencyViewController()
 //    let firstVC = FirstViewController()
 //    let navigationController = UINavigationController(rootViewController: firstVC)
     window?.rootViewController = viewController

0880083505600347567c9683b202b7a98426172c
Kanagasabapathy <trksabapathy7@gmail.com>
2023-08-27 16:57:19 +0530

    Re-Grouped the Components as per the Architecture Currently, this project consists of UIKit and SwiftUI and how it performs with Architectures such as MVVM and TCA

---
+++ CurrencyConverter/MVVM/Model/Currency.swift
@@ -0,0 +1,43 @@
+//
+//  Currency.swift
+//  CurrencyConverter
+//
+//  Created by kanagasabapathy on 26/06/23.
+//
+
+import Foundation
+
+// Active
+// https://pastebin.com/raw/Nq1KvHjZ
+
+ struct CurrencyData: Decodable, Hashable {
+  let success: Bool
+  let timestamp: Int
+  let base: String
+  let date: String
+  let rates: [String: Double]
+ }
+
+struct TableViewData: Identifiable, Hashable, Decodable {
+  let id = UUID()
+  var base: String
+  var currencyCode: String
+  var currencyName: String
+  var currencyValue: Double
+  var currencySymbol: String
+}
+
+struct Currency {
+  var code: String
+  var symbol: String
+}
+
+/*
+ 1 euro is the base
+ = 90.67 INR
+ = 1.09 USD
+
+ 1 USD is the base
+ =  if (1.09 USD = 90.67 INR) what is 1 USD = 83.18 INR
+         90.67/1.09 * 1
+ */
---
+++ CurrencyConverter/MVVM/SwiftUIComponents/CurrencySwiftUIViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CurrencySwiftUIViewController.swift
+//  CurrencyConverter
+//
+//  Created by kanagasabapathy on 10/08/23.
+//
+
+import UIKit
+import SwiftUI
+
+class CurrencySwiftUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      // Added HostingController to extract the CurrencySwiftUIView
+//      let hostingVC = UIHostingController(rootView: CurrencySwiftUIView())
+      let hostingVC = UIHostingController(rootView: MainView())
+
+      guard let hostingVC = hostingVC as? UIHostingController else {
+        return
+      }
+      guard let swiftUIView = hostingVC.view else {
+        return
+      }
+      swiftUIView.translatesAutoresizingMaskIntoConstraints = false
+      // add HostingVC to destination VC
+      addChild(hostingVC)
+      // add SwiftUIView to view's Hierarchy
+      view.addSubview(swiftUIView)
+      // Adding Constraints to Layout Guide
+      swiftUIView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                         leading: view.safeAreaLayoutGuide.leadingAnchor,
+                         bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                         trailing: view.safeAreaLayoutGuide.trailingAnchor)
+      // To notify if that the transition is complete
+      hostingVC.didMove(toParent: self)
+    }
+}

b55c0e4d3f325a6567ab4abd9801e6aa5827affe
Kanagasabapathy <trksabapathy7@gmail.com>
2023-09-07 19:48:18 +0530

    Formatted the source code in various files in SwiftUI

---
+++ CurrencyConverter/MVVM/SwiftUI/CurrencyRowView.swift
@@ -0,0 +1,33 @@
+//
+//  CurrencyRowView.swift
+//  CurrencyConverter
+//
+//  Created by kanagasabapathy on 10/08/23.
+//
+
+import SwiftUI
+
+struct CurrencyRowView: View {
+  @State var tableViewData: TableViewData
+    var body: some View {
+      HStack(alignment: .bottom) {
+        VStack(alignment: .leading) {
+          Text(tableViewData.currencyName ).font(.caption)
+          Text(tableViewData.currencyCode).font(.subheadline).bold()
+        }
+        Spacer()
+        Text(String(format: "%.2f", tableViewData.currencyValue.rounded(toPlaces: 2))).font(.headline)
+      }.padding()
+    }
+}
+
+struct CurrencyRowView_Previews: PreviewProvider {
+    static var previews: some View {
+      let tableViewData = TableViewData(base: "EUR",
+                                        currencyCode: "EUR",
+                                        currencyName: "Euro",
+                                        currencyValue: 10.222,
+                                        currencySymbol: "$")
+        CurrencyRowView(tableViewData: tableViewData)
+    }
+}

681c14ee18f75bd7f19b5017dd22dff9df56d3ba
Kanagasabapathy <trksabapathy7@gmail.com>
2023-09-09 01:54:05 +0530

    - Modified the Model class as per the new third-party API to fetch the Currencies - https://app.exchangerate-api.com/ - Updated swiftlint.yml to accept the identifier name with allowed symbols and lowercase. - Integrated the new API to the RequestService Class - Added JSONSerialization for testing purpose

--- CurrencyConverter/MVVM/Model/Currency.swift
+++ CurrencyConverter/MVVM/Model/Currency.swift
@@ -9,25 +9,35 @@ import Foundation
 
 // Active
 // https://pastebin.com/raw/Nq1KvHjZ
+//https://v6.exchangerate-api.com/v6/c1a4fe51ecfcebb4ddda0835/latest/INR
+struct CurrencyData: Decodable, Hashable {
+//    let success: Bool
+//    let timestamp: Int
+//    let base: String
+//    let date: String
+//    let rates: [String: Double]
 
- struct CurrencyData: Decodable, Hashable {
-  let success: Bool
-  let timestamp: Int
-  let base: String
-  let date: String
-  let rates: [String: Double]
- }
+    let result: String
+    let documentation: String
+    let terms_of_use: String
+    let time_last_update_unix: Int
+    let time_last_update_utc: String
+    let time_next_update_unix: Int
+    let time_next_update_utc: String
+    let base_code: String
+    let conversion_rates: [String: Double]
+}
 
 struct TableViewData: Equatable, Identifiable, Hashable, Decodable {
-  var id = UUID()
-  var base: String
-  var currencyCode: String
-  var currencyName: String
-  var currencyValue: Double
-  var currencySymbol: String
+    var id = UUID()
+    var base: String
+    var currencyCode: String
+    var currencyName: String
+    var currencyValue: Double
+    var currencySymbol: String
 }
 
 struct Currency {
-  var code: String
-  var symbol: String
+    var code: String
+    var symbol: String
 }

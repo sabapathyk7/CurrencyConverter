7c69d247b44b896856866afd297751ee8b0cfa83
Kanagasabapathy <trksabapathy7@gmail.com>
2023-09-11 22:43:35 +0530

    Added ProgressView to SwiftUI ContentView Added delay of the ProgressView for about 5 seconds to load the values to the listView

--- CurrencyConverter/MVVM/SwiftUI/CurrencySwiftUIView.swift
+++ CurrencyConverter/MVVM/SwiftUI/CurrencySwiftUIView.swift
@@ -9,28 +9,37 @@ import SwiftUI
 
 struct CurrencySwiftUIView: View {
     @ObservedObject var viewModel = CurrencyViewModel()
+    @State var isHidden = false
     var body: some View {
         VStack {
-            HStack {
-              TextField("Enter text", text: $viewModel.enteredAmount)
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
-                Spacer()
-                Picker("Currency", selection: $viewModel.selectedBaseCurrency) {
-                    Text(viewModel.selectedBaseCurrency).tag(viewModel.selectedBaseCurrency)
-                  ForEach(viewModel.tableViewDataArray, id: \.self) {
-                      Text($0.currencyCode).tag($0.currencyCode)
+            if isHidden {
+                HStack {
+                    TextField("Enter text", text: $viewModel.enteredAmount)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                    Spacer()
+                    Picker("Currency", selection: $viewModel.selectedBaseCurrency) {
+                        Text(viewModel.selectedBaseCurrency).tag(viewModel.selectedBaseCurrency)
+                        ForEach(viewModel.tableViewDataArray, id: \.self) {
+                            Text($0.currencyCode).tag($0.currencyCode)
+                        }
                     }
+                    .pickerStyle(.menu)
                 }
-                .pickerStyle(.menu)
+                .progressViewStyle(.circular)
+                Spacer()
+                CurrencyListView(tableViewData: $viewModel.tableViewDataArray)
+            } else {
+                ProgressView("Loading")
             }
-            Spacer()
-          CurrencyListView(tableViewData: $viewModel.tableViewDataArray)
         }
         .onAppear {
             Task {
                 viewModel.callFetchCurrencyDataSwiftUI(base: viewModel.selectedBaseCurrency)
             }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: {
+                isHidden = true
+            })
         }
     }
 }

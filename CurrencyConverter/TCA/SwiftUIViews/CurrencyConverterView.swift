3be3160d68c928d59f4503b1e9666d53a072f1ee
Kanagasabapathy <trksabapathy7@gmail.com>
2023-09-13 00:20:12 +0530

    Added ProgressView to the TCA SwiftUI

--- CurrencyConverter/TCA/SwiftUIViews/CurrencyConverterView.swift
+++ CurrencyConverter/TCA/SwiftUIViews/CurrencyConverterView.swift
@@ -10,40 +10,48 @@ import ComposableArchitecture
 
 struct CurrencyConverterView: View {
     let store: StoreOf<CurrencyConverter>
+    @State var isHidden = false
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             VStack {
-                HStack {
-                    TextField(
-                        "Enter text",
-                        text: viewStore.binding(
-                            get: \.priceQuantityEntered,
-                            send: { .quantityTextFieldEntered($0) }
+                if isHidden {
+                    HStack {
+                        TextField(
+                            "Enter text",
+                            text: viewStore.binding(
+                                get: \.priceQuantityEntered,
+                                send: { .quantityTextFieldEntered($0) }
+                            )
                         )
-                    )
-                    .keyboardType(.numberPad)
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
-                    Spacer()
-                    Picker("Currency",
-                           selection: viewStore.binding(
-                            get: \.selectedBaseCurrency,
-                            send: { .countryCodePickerSelected($0) })
-                    ) {
-                        Text(viewStore.selectedBaseCurrency).tag(viewStore.selectedBaseCurrency)
-                        ForEach(viewStore.currencies) {
-                            Text($0.currencyCode).tag($0.currencyCode)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                        Spacer()
+                        Picker("Currency",
+                               selection: viewStore.binding(
+                                get: \.selectedBaseCurrency,
+                                send: { .countryCodePickerSelected($0) })
+                        ) {
+                            Text(viewStore.selectedBaseCurrency).tag(viewStore.selectedBaseCurrency)
+                            ForEach(viewStore.currencies) {
+                                Text($0.currencyCode).tag($0.currencyCode)
+                            }
                         }
+                        .pickerStyle(.menu)
                     }
-                    .pickerStyle(.menu)
-                }
-                Spacer()
-                List(viewStore.currencies) { tableViewData in
-                    CurrencyRowView(tableViewData: tableViewData)
+                    Spacer()
+                    List(viewStore.currencies) { tableViewData in
+                        CurrencyRowView(tableViewData: tableViewData)
+                    }
+                } else {
+                    ProgressView("Loading")
                 }
             }
             .onAppear {
                 viewStore.send(.onAppear)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: {
+                    isHidden = true
+                })
             }
         }
     }
